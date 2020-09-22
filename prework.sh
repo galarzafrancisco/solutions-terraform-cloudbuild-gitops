666dbe59f0d0585d6ec6be25256745cab75e69f3
Francisco Galarza <franciscogalarza@MBP-13-2019.local>
2020-09-22 15:05:03 +1000

    Added prework script

--- prework.sh
+++ prework.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Based on Managing Infrastructure as Code, by Google
+# https://cloud.google.com/solutions/managing-infrastructure-as-code
+
 # Get Google Cloud project id
 PROJECT_ID=$(gcloud config get-value project)
 
@@ -20,3 +23,10 @@ if [ "$(uname)" = "Linux" ]; then
     sed -i s/PROJECT_ID/$PROJECT_ID/g environments/*/terraform.tfvars
     sed -i s/PROJECT_ID/$PROJECT_ID/g environments/*/backend.tf
 fi
+
+
+# Grant permissions to the Cloud Build service account
+CLOUDBUILD_SA="$(gcloud projects describe $PROJECT_ID \
+    --format 'value(projectNumber)')@cloudbuild.gserviceaccount.com"
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member serviceAccount:$CLOUDBUILD_SA --role roles/editor
